64e5a6ec7f60460898add3f38050671603d7e0d2
flatberg <arnar.flatberg@ntnu.no>
2019-06-28 12:35:13 +0200

    AAc

--- rules/quant/scripts/make_manifest_qiime2.py
+++ rules/quant/scripts/make_manifest_qiime2.py
@@ -4,37 +4,40 @@
 import sys
 import yaml
 import os
+import argparse
+import re
+import warnings
 
-helptext = '\nUSAGE: make_manifest_qiime2.py /path/to/config.yaml /path/to/manifest.csv\n'
 
-def main(conffile, manifest):
-    with open(conffile, 'r') as cfile:
-        conf = yaml.load(cfile)
-    paired_end = len(conf['read_geometry']) > 1
-    fastq_dir = os.path.abspath(conf['fastq_dir'])
-    if paired_end:
-        header = ['sample-id', 'forward-absolute-filepath', 'reverse-absolute-filepath']
-    else:
-        header = ['sample-id', 'absolute-filepath']
-
-    with open(manifest, 'w+') as manf:
-        manf.write('\t'.join(header) + '\n')
-
-        for s in conf['samples'].keys():
-            sample = conf['samples'][s]
-            r1_abspath = os.path.join(fastq_dir, sample['R1'])
-            if paired_end:
-                r2_abspath = os.path.join(fastq_dir, sample['R2'])
-                manf.write('{}\t{}\t{}\n'.format(s, r1_abspath, r2_abspath))
-            else:
-                manf.write('{}\t{}\n'.format(s, r1_abspath))
+parser = argparse.ArgumentParser(description='create a valid qiime2 manifest file', formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('input', help='fastq files formatted as Sample_ID_R1[2].fastq[.gz]', nargs='+')
 
 if __name__ == '__main__':
-    try:
-        conffile = sys.argv[1]
-        manifest = sys.argv[2]
-    except:
-        print(helptext)
-        sys.exit()
+    args = parser.parse_args()
+    
+    SAMPLES = set()
+    patt = re.compile('(.*)_R[1-2].fastq')
+    for fn in args.input:
+        m = patt.match(os.path.basename(fn))
+        if m:
+            SAMPLES.add(m.groups()[0])
+        else:
+            raise ValueError('failed to identify Sample_ID from file: {}'.format(fn))
+
+    n_fastq, n_samples = len(args.input), len(SAMPLES)
+    if 2 * n_samples == n_fastq: #paired end
+        header = ['sample-id', 'forward-absolute-filepath', 'reverse-absolute-filepath']
+    elif n_samples == n_fastq: # single end
+        header = ['sample-id', 'forward-absolute-filepath']
+    else:
+        raise ValueError('fastq files contains non-unique sample-ids or is a mix of paired and single end reads!')
+
+    sys.stdout.write('\t'.join(header) + '\n')
+
+    for sample_id in SAMPLES:
+        els = [sample_id]
+        for fn in args.input:
+            if os.path.basename(fn).startswith(sample_id):
+                els.append(os.path.abspath(fn))
+        sys.stdout.write('\t'.join(els) + '\n')
         
-    main(conffile, manifest)
